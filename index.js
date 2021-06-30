cb2954e8ce745dcfe8a58d54789a3bdf66c175fe
inthemonzon <inthemonzon@yahoo.com>
2021-06-29 20:39:25 -0400

    commit tree 5f25a92d76d2772e25f17c1b3f00254fdddbe788

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+
+const returnFirstTwoDrivers = (array) => array.slice(0,2)
+
+const returnLastTwoDrivers = (array) => array.slice(array.length - 2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (int) => (fare) => int * fare;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (array, selectingDrivers) {
+    let randomNumber = Math.random();
+    Math.round(randomNumber);
+    return selectingDrivers(array);
+}
